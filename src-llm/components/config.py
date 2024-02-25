9c1644382fc660355370adb0c29606d11a55f913
JAGPREET SINGH <singhjagpreet096@gmail.com>
2024-02-24 19:15:12 -0500

    add shortcut connection

---
+++ src-llm/components/config.py
@@ -0,0 +1,9 @@
+GPT_CONFIG_124M = {
+    "vocab_size" : 50257, # vocabulary size
+    "ctx_len" : 1024,     # context length
+    "emb_dim" : 768,      # embedding dimesension
+    "n_head" : 12,        # number of attention heads
+    "n_layers" : 12,      # number of layers
+    "drop_rate" : 0.1,    # Dropout rate
+    "qkv_bias" : False
+}
---
+++ src-llm/components/transfomerblock.py
@@ -0,0 +1,12 @@
+import torch.nn as nn
+
+class TransformerBlock(nn.Module):
+    
+    def __init__(self, cfg):
+        super().__init__()
+        # a placeholder
+
+    def forward(self, x):
+        ## this block does nothing just returns its input
+        return x
+    
