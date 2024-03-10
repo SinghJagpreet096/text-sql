6d598c58337509e4cb45dc6e4def101cf0a86548
JAGPREET SINGH <singhjagpreet096@gmail.com>
2024-03-09 19:53:45 -0500

    added nsql 350M

---
+++ src-llm/app2.py
@@ -0,0 +1,63 @@
+from transformers import AutoTokenizer, AutoModelForCausalLM
+import chainlit as cl
+import re
+tokenizer = AutoTokenizer.from_pretrained("NumbersStation/nsql-350M")
+model = AutoModelForCausalLM.from_pretrained("NumbersStation/nsql-350M")
+
+
+
+# text = """CREATE TABLE stadium (
+#     stadium_id number,
+#     location text,
+#     name text,
+#     capacity number,
+#     highest number,
+#     lowest number,
+#     average number
+# )
+
+# CREATE TABLE singer (
+#     singer_id number,
+#     name text,
+#     country text,
+#     song_name text,
+#     song_release_year text,
+#     age number,
+#     is_male others
+# )
+
+# CREATE TABLE concert (
+#     concert_id number,
+#     concert_name text,
+#     theme text,
+#     stadium_id text,
+#     year text
+# )
+
+# CREATE TABLE singer_in_concert (
+#     concert_id number,
+#     singer_id text
+# )
+
+# -- Using valid SQLite, answer the following questions for the tables provided above.
+
+# -- What is the maximum, the average, and the minimum capacity of stadiums ?
+
+# SELECT"""
+
+@cl.on_message
+async def main(message: cl.Message):
+
+    text = message.content
+    input_ids = tokenizer(text, return_tensors="pt").input_ids
+
+    generated_ids = model.generate(input_ids, max_length=500)
+    pred = (tokenizer.decode(generated_ids[0], skip_special_tokens=True))
+    # Send a response back to the user
+    if "SELECT" in pred:
+        pred = pred.split("SELECT")[1]
+    pred = "SELECT " + re.sub('"""', "",pred)
+    
+    await cl.Message(
+        content=pred,
+    ).send()
